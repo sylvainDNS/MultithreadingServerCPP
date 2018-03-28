0d92128b9ae27e07b350781d899587330705967a
Sylvain <sylvain.denyse@gmail.com>
2018-03-28 13:03:36 +0200

    multi

--- server.cpp
+++ server.cpp
@@ -2,11 +2,31 @@
 #include "SocketException.h"
 #include <string>
 #include <iostream>
+#include <thread>
 
 using namespace std;
 
-int main (int argc, char* argv[]) {
-  if(argc !=2) {
+void request(ServerSocket new_sock)
+{
+  try
+  {
+    while (true)
+    {
+      string data;
+      new_sock >> data;
+      new_sock << data;
+    }
+  }
+  catch (SocketException &)
+  {
+    perror("Request error");
+  }
+}
+
+int main(int argc, char *argv[])
+{
+  if (argc != 2)
+  {
     cout << "invalid number of arguments: call with `./server port`" << endl;
     return 1;
   }
@@ -15,24 +35,21 @@ int main (int argc, char* argv[]) {
 
   cout << "running....\n";
 
-  try {
-    ServerSocket server ( port );
+  try
+  {
+    ServerSocket server(port);
 
-    while ( true ) {
-
+    while (true)
+    {
       ServerSocket new_sock;
-      server.accept ( new_sock );
-
-      try {
-        while ( true ) {
-          string data;
-          new_sock >> data;
-          new_sock << data;
-        }
-      } catch ( SocketException& ) {}
-
+      server.accept(new_sock);
+      // cout << "Socket created" << endl;
+      thread reqThread(request, new_sock);
+      reqThread.join();
     }
-  } catch ( SocketException& e ) {
+  }
+  catch (SocketException &e)
+  {
     cout << "Exception was caught:" << e.description() << "\nExiting" << endl;
   }
 
