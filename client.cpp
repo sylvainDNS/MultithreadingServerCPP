21e991e64f42d9d8fda7052c94429d3debd8e3be
JacquesLesur <lesurjacques@gmail.com>
2018-04-06 17:13:14 +0200

    Front psudo plus msg
    Demande de pseudo, cmd USER et MSG fait
    Pseudo inséré dans la string, à voir si il y a pas mieux à faire

--- client.cpp
+++ client.cpp
@@ -6,8 +6,10 @@
 
 using namespace std;
 
-int main (int argc, char* argv[]) {
-  if(argc != 3) {
+int main(int argc, char *argv[])
+{
+  if (argc != 3)
+  {
     cout << "invalid number of arguments: call with `./client IP port`" << endl;
     return 1;
   }
@@ -15,18 +17,60 @@ int main (int argc, char* argv[]) {
   string host = argv[1];
   int port = atoi(argv[2]);
 
-  try {
-    ClientSocket client_socket ( host, port );
-    string reply;
 
-    try {
-      client_socket << "Test message.";
-      client_socket >> reply;
-    } catch ( SocketException& ) {}
+  try
+  {
+    ClientSocket client_socket(host, port);
+    string saisiePseudo;
+     string cmdUser;
+     string Pseudo;
 
-    cout << "We received this response from the server:\n\"" << reply << "\"\n";;
+    do
+    {
+      cout << "Donnez moi votre pseudo grace à la commande USER" << endl << endl;
+      getline(cin, saisiePseudo);
+      
+    
+      int posPoint1= saisiePseudo.find(' ',0);
+      Pseudo= saisiePseudo.substr(posPoint1+1,saisiePseudo.length());
+      cmdUser = saisiePseudo.substr(0,posPoint1);
+      
+     
+      
+    }while(cmdUser != "USER");
+    cout <<"Bonjour " << Pseudo << " ! Bienvenu sur le Chat :)" << endl << endl;
 
-  }  catch ( SocketException& e ) {
+    while(true){
+      string reply;
+      string saisie;
+      getline(cin, saisie);
+      int posSpace= saisie.find(' ',0);
+      string cmdMSG = saisie.substr(0,posSpace);
+      string msg= saisie.substr(posSpace+1,saisie.length());
+      //  cout << cmdMSG << endl;
+      //  cout << msg << endl;
+      
+      if(cmdMSG == "MSG")
+      {
+        try
+        {
+          client_socket << msg;
+          client_socket >> reply;
+        }
+        catch (SocketException &)
+        {
+          cout << "error";
+        }
+        cout << "We received this response from the server:\n"<< Pseudo << ": "<< reply <<  "\n" << endl ;
+      }
+      else{
+        cout << "Met la commande MSG avant ton msg genius !" << endl;
+      }
+    }
+    
+  }
+  catch (SocketException &e)
+  {
     cout << "Exception was caught:" << e.description() << endl;
   }
 
